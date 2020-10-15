37e695beaa4477d2009cd54e6b09e3a81bc8243f
JQartas <jcuartas0611@gmail.com>
2020-10-14 20:21:20 -0500

    first implementation of create_matrix

--- matriz_porosidad.py
+++ matriz_porosidad.py
@@ -1,7 +1,24 @@
 import numpy as np
 
-def crear_matriz (n,m,phi_m, phi_f):
+def create_matrix (n,m,phi_m, phi_f):
   '''
-    Devuelve matriz de porosidad de n x m con las 
-    constantes de phi_m y phi_f
+    Returns a matrix of porosity of n x m 
+    with the constants of phi_m and phi_f
   '''
+  inter = []
+  fracture = []
+  matrix=[]
+  for i in range(n):
+      if i % 2 == 0:
+          inter.append(phi_m)
+      else:
+          inter.append(phi_f)
+      fracture.append(phi_f)
+
+  for j in range(m):
+      if j % 2 == 0:
+          matrix.append(inter)
+      else:
+          matrix.append(fracture)
+  
+  return np.array(matrix)
--- matriz_porosidad_test.py
+++ matriz_porosidad_test.py
@@ -1,19 +1,19 @@
 import numpy as np
 import unittest
-from matriz_porosidad import crear_matriz
+from matriz_porosidad import create_matrix
 
 class MatrizPorosidadTests(unittest.TestCase):
   def test_has_to_be_list(self):
     '''
       Should returns a python list
     '''
-    self.assertTrue(type(crear_matriz(6, 5, 'm', 'f')) is np.ndarray)
+    self.assertTrue(type(create_matrix(6, 5, 'm', 'f')) is np.ndarray)
   
   def test_has_given_size(self):
     '''
       Should returns a matrix with certain size given parameters
     '''
-    matriz = crear_matriz(6, 5, 'm', 'f')
+    matriz = create_matrix(6, 5, 'm', 'f')
 
     self.assertEqual(len(matriz), 5)
     self.assertEqual(len(matriz[0]), 6)
@@ -22,7 +22,7 @@ class MatrizPorosidadTests(unittest.TestCase):
     '''
       Should return an empty array
     '''
-    matriz = crear_matriz(0, 0, 'm', 'f')
+    matriz = create_matrix(0, 0, 'm', 'f')
 
     self.assertEqual(len(matriz), len([]))
 
@@ -32,7 +32,7 @@ class MatrizPorosidadTests(unittest.TestCase):
     '''
     char1 = 'm'
     char2 = 'f'
-    matriz = crear_matriz(10, 10, char1, char2)
+    matriz = create_matrix(10, 10, char1, char2)
     correct = True
 
     for j in matriz:
@@ -47,12 +47,12 @@ class MatrizPorosidadTests(unittest.TestCase):
     '''
       Should return an error if the matrix doesn't receive a number in its dimensions
     '''
-    self.assertEqual(crear_matriz('ex', 10, 'm', 'f'), "n debe ser un número entero")
+    self.assertEqual(create_matrix('ex', 10, 'm', 'f'), "n debe ser un número entero")
 
   def test_negative_size(self):
     '''
       Should return an error if the matrix receive a negative number in its dimensions
     '''
-    self.assertRaises(IndexError, crear_matriz(-1, -2, 'm', 'f'), 1, '1')
+    self.assertRaises(IndexError, create_matrix(-1, -2, 'm', 'f'), 1, '1')
 
 unittest.main()
