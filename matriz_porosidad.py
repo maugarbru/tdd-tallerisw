34b87844140820fda242035bd456685be051abea
Mauricio G <mauriciogarzonb@gmail.com>
2020-10-14 20:50:51 -0500

    Refactor of create_matrix function

--- matriz_porosidad.py
+++ matriz_porosidad.py
@@ -5,20 +5,18 @@ def create_matrix (n,m,phi_m, phi_f):
     Returns a matrix of porosity of n x m 
     with the constants of phi_m and phi_f
   '''
+  assert type(n) == type(2) and n >= 0, "n must be an integer"
+  assert type(m) == type(2) and m >= 0, "m must be an integer"
+  matrix = []
   inter = []
-  fracture = []
-  matrix=[]
+  fracture = [phi_f]*n
+
   for i in range(n):
-      if i % 2 == 0:
-          inter.append(phi_m)
-      else:
-          inter.append(phi_f)
-      fracture.append(phi_f)
+      value_to_add = phi_m if i % 2 == 0 else phi_f
+      inter.append(value_to_add)
 
   for j in range(m):
-      if j % 2 == 0:
-          matrix.append(inter)
-      else:
-          matrix.append(fracture)
-  
+      row_to_add = inter if j % 2 == 0 else fracture
+      matrix.append(row_to_add)
+      
   return np.array(matrix)
