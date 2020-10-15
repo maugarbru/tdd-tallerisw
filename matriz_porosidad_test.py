a951d663a6f25048ff060171a4d1c79cf42d4efb
Santiago Betancur <scb9908@gmail.com>
2020-10-14 19:34:58 -0500

    Implemented tests

--- matriz_porosidad_test.py
+++ matriz_porosidad_test.py
@@ -17,5 +17,30 @@ class MatrizPorosidadTests(unittest.TestCase):
     self.assertEqual(len(matriz), 6)
     self.assertEqual(len(matriz[0]), 5)
 
+  def test_with_no_size(self):
+    '''
+      Should return an empty array
+    '''
+    matriz = crear_matriz(0, 0, 'm', 'f')
+
+    self.assertEqual([matriz, []])
+
+  def test_has_correct_data(self):
+    '''
+      Only filled with correct data
+    '''
+    char1 = 'm'
+    char2 = 'f'
+    matriz = crear_matriz(10, 10, char1, char2)
+    correct = True
+
+    for j in matriz:
+      for i in j:
+        print(i, char1, char2, i == char1, i == char2)
+        if not i == char1 and not i == char2:
+          correct = False
+          continue
+
+    self.assertEqual(correct, True)
 
 unittest.main()
